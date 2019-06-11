86d9f1c236c3e420548669544df705ce73ad2085
hubert.struminski <hubert.struminski@gmail.com>
2019-06-11 16:02:12 +0200

    configuration react js project, configuration store

--- sudoku-react-js/src/App.js
+++ sudoku-react-js/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Layout/Header';
+import Sudoku from './components/Sudoku';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store} >
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/" component={Sudoku} />
+          <Route exact path="/sudoku" component={Sudoku} />
+
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ sudoku-react-js/src/components/Sudoku.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Sudoku extends React.Component {
+    render() {
+        return (
+            <div>Sudoku</div>
+        );
+    }
+}
+
+export default Sudoku;
