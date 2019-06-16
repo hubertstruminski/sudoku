d08282a23d4acabd6f50f39dbaad9a190936e56a
hubert.struminski <hubert.struminski@gmail.com>
2019-06-16 15:44:13 +0200

    add StatisticsRepository interface

---
+++ sudoku-java/src/main/java/com/example/demo/repository/StatisticsRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Statistics;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StatisticsRepository extends CrudRepository<Statistics, Long> {
+}
