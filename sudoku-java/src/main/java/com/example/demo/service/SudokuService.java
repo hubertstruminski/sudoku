36accdf8445f899e3ee6a3d98c5a1f2feeab7f2e
hubert.struminski <hubert.struminski@gmail.com>
2019-07-08 21:02:03 +0200

    remove dead code, clean code

--- sudoku-java/src/main/java/com/example/demo/service/SudokuService.java
+++ sudoku-java/src/main/java/com/example/demo/service/SudokuService.java
@@ -2,8 +2,6 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
-
 @Service
 public class SudokuService {
 
@@ -21,23 +19,4 @@ public class SudokuService {
         }
         return result;
     }
-
-//    public static void main(String[] args) {
-//        Random random = new Random();
-//        int[] entireArray = new int[81];
-//
-//        for(int i=0; i<81; i++) {
-//            int randomValue = random.nextInt(9) + 1;
-//            entireArray[i] = randomValue;
-//        }
-//
-//        int[][] result = convertArray(entireArray);
-//
-//        for(int i=0; i<result.length; i++) {
-//            for(int j=0; j<result[i].length; j++) {
-//                System.out.print(result[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
 }
--- sudoku-react-js/src/components/Result.js
+++ sudoku-react-js/src/components/Result.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 class Result extends React.Component {
     constructor(props) {
         super(props);
-
     }
 
     render() {
